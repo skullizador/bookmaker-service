ca54ffe5af3e0fe5ef05dcc4d2eca2d1a50915a7
Motinha <motinha11@outlook.pt>
2022-12-27 23:35:44 +0000

    HA-40: Add 'Create Bookmaker' feature;

--- src/Domain/AggregateModels/Bookmaker/Repository/IBookmakerRepository.cs
+++ src/Domain/AggregateModels/Bookmaker/Repository/IBookmakerRepository.cs
@@ -9,6 +9,8 @@
 
 namespace BookmakerService.Domain.AggregateModels.Bookmaker.Repository
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using BookmakerService.Domain.SeedWork;
 
     /// <summary>
@@ -17,5 +19,13 @@ namespace BookmakerService.Domain.AggregateModels.Bookmaker.Repository
     /// <seealso cref="IRepository{Bookmaker}" />
     public interface IBookmakerRepository : IRepository<Bookmaker>
     {
+        /// <summary>
+        /// Gets the bookmaker by base URL and country asynchronous.
+        /// </summary>
+        /// <param name="baseUrl">The base URL.</param>
+        /// <param name="country">The country.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        Task<Bookmaker> GetBookmakerByBaseUrlAndCountryAsync(string baseUrl, string country, CancellationToken cancellationToken);
     }
 }
