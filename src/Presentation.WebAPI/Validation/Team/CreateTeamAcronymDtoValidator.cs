964519cdcef658c35c6a3a0df98c32ac5bc57a44
Motinha <motinha11@outlook.pt>
2023-01-10 18:46:29 +0000

    HA-72: Add input dtos validations;

---
+++ src/Presentation.WebAPI/Validation/Team/CreateTeamAcronymDtoValidator.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CreateTeamAcronymDtoValidator.cs" company="HumbleBets">
+//     Copyright (c) HumbleBets. All rights reserved.
+// </copyright>
+// <summary>
+// CreateTeamAcronymDtoValidator
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BookmakerService.Presentation.WebAPI.Validation.Team
+{
+    using BookmakerService.Presentation.WebAPI.Dtos.Input.Team;
+    using FluentValidation;
+
+    /// <summary>
+    /// <see cref="CreateTeamAcronymDtoValidator"/>
+    /// </summary>
+    /// <seealso cref="AbstractValidator{CreateTeamAcronymDto}"/>
+    public class CreateTeamAcronymDtoValidator : AbstractValidator<CreateTeamAcronymDto>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CreateTeamAcronymDtoValidator"/> class.
+        /// </summary>
+        public CreateTeamAcronymDtoValidator()
+        {
+            this.RuleFor(x => x.Acronym)
+                .NotEmpty()
+                    .WithMessage("The Acronym shouldn't be empty.")
+                .NotNull()
+                    .WithMessage("The Acronym shouldn't be null.")
+                .MaximumLength(10)
+                    .WithMessage("The Acronym shouldn't be longer than 10 characters.");
+        }
+    }
+}
